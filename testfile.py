4d82ebecbeb46e2bc433ff6d6fb129214959202b
Cogin <cevinmontgomery05@gmail.com>
2021-12-29 08:25:20 -0600

    oo yea testing time

--- testfile.py
+++ testfile.py
@@ -1,1 +1,10 @@
-print('man I havent even started yet ill be honest')
+import board
+import neopixel
+import time
+
+pixels = neopixel.Neopixel(board.D18, 50, brightness = 1)
+pixels.fill(0,0,0)
+
+time.sleep(2)
+pixels.fill(0,0,255)
+pixels.show()
