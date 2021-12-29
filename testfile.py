0749fd466e0d538726c7b8eea082f82c0f9ae2f0
Cogin <cevinmontgomery05@gmail.com>
2021-12-29 08:35:59 -0600

    epic

--- testfile.py
+++ testfile.py
@@ -2,7 +2,7 @@ import board
 import neopixel
 import time
 
-pixels = neopixel.Neopixel(board.D18, 50, brightness = 1)
+pixels = neopixel.NeoPixel(board.D18, 1, brightness = 1)
 pixels.fill(0,0,0)
 
 time.sleep(2)
